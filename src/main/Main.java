5884009dc90e542b55ee63b898044fb968161981
Aakhya <aakhya.swamiwal_cs18@gla.ac.in>
2019-08-22 15:17:12 +0530

    how to get output

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String userName = "";
+        String userName = scanner.nextLine().trim();
+        System.out.println("Welcome," + userName + "!");
         scanner.close();
 
     }

aedb088500a41aa54d3bdcf93ace7fb3e8f002a5
Aakhya <aakhya.swamiwal_cs18@gla.ac.in>
2019-08-22 15:03:03 +0530

    create main method

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,7 @@
 package main;
-import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner value=new Scanner(System.in);
-        System.out.println("enter your name");
-        String name=value.next();
-        System.out.println(name);
-
 
     }
 

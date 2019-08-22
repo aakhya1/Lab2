55576696ba07bbf4c5a0506b863b03478471a38e
Aakhya <aakhya.swamiwal_cs18@gla.ac.in>
2019-08-22 15:14:29 +0530

    how to input string

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,12 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String userName = "";
+        scanner.close();
 
     }
 
